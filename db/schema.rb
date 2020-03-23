e827143000fba8b7100cfcba1064b11561fb1d01
bird-grid2 <bird2.one.regend@gmail.com>
2020-03-23 22:57:32 +0900

    modified  input-form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200321060954) do
+ActiveRecord::Schema.define(version: 20200322031429) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "group_id"
@@ -32,6 +32,17 @@ ActiveRecord::Schema.define(version: 20200321060954) do
     t.index ["user_id"], name: "index_groups_on_user_id", using: :btree
   end
 
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "content"
+    t.string   "image"
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
@@ -50,4 +61,6 @@ ActiveRecord::Schema.define(version: 20200321060954) do
   add_foreign_key "group_users", "users"
   add_foreign_key "groups", "groups"
   add_foreign_key "groups", "users"
+  add_foreign_key "messages", "groups"
+  add_foreign_key "messages", "users"
 end
