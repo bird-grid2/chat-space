587065932c23417c12e51681e09c8bb43816f06c
bird-grid2 <bird2.one.regend@gmail.com>
2020-04-04 16:10:39 +0900

    modified_create_json_jbuilder

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,17 @@
+$(function(){
+  
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var url = $(this).attr('action');
+    var formData = new FormData(this);
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  });
+});
