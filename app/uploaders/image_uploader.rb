97d4e2dcfcefd219b39a5a749d15d949baebcbc2
bird-grid2 <bird2.one.regend@gmail.com>
2020-04-12 20:01:46 +0900

    modified_image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  # storage :fog
+  process resize_to_fit: [800, 800]
+  
+  # Choose what kind of storage to use for this uploader:
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -41,5 +44,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-  process resize_to_fit: [800, 800]
 end
