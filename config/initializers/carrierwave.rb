cc7cc86e4baf34258242e4dd98e5aea79d140b9f
bird-grid2 <bird2.one.regend@gmail.com>
2020-04-12 21:39:04 +0900

    modified carrierwave.rb_2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory = 'chatspace-initial'
+  config.fog_directory  = 'chatspace-initial'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace-initial'
 end
